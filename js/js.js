162b3a74cbf25abef98d6f7ec39bb69e95d1b3b6
DavidVillalba <davidvillalba92@gmail.com>
2018-01-20 15:13:10 +0100

    DavidVillalba

--- js/js.js
+++ js/js.js
@@ -1,23 +1,60 @@
 window.onload = function () {
+    document.getElementById("menuMovil").onclick = menuMovil;
     document.getElementById("inicio").onclick = inicio;
     document.getElementById("info").onclick = info;
     document.getElementById("ubicacion").onclick = ubicacion;
+     document.getElementById("about").onclick = about;
 };
 
+function menuMovil() {
+    if (document.getElementById("menuMovil").style.position === "initial") {
+        document.getElementById("info").style.display = "none";
+        document.getElementById("ubicacion").style.display = "none";
+        document.getElementById("about").style.display = "none";
+        document.getElementById("menuMovil").style.position = "absolute";
+    } else {
+        document.getElementById("inicio").style.display = "block";
+        document.getElementById("info").style.display = "block";
+        document.getElementById("ubicacion").style.display = "block";
+        document.getElementById("about").style.display = "block";
+        document.getElementById("menuMovil").style.position = "initial";
+        document.getElementById("menuMovil").style.textAlign = "left";
+    }
+
+}
+
 function inicio() {
-    document.getElementById("inicio").innerHTML = "···INICIO···";
-    document.getElementById("info").innerHTML = "INFORMACIÓN";
-    document.getElementById("ubicacion").innerHTML = "UBICACIÓN";
+    if (document.getElementById("menuMovil").style.position === "initial") {
+        document.getElementById("info").style.display = "none";
+        document.getElementById("ubicacion").style.display = "none";
+        document.getElementById("about").style.display = "none";
+        document.getElementById("menuMovil").style.position = "absolute";
+    } 
 }
 
 function info() {
-    document.getElementById("inicio").innerHTML = "INICIO"
-    document.getElementById("info").innerHTML = "···INFORMACIÓN···";
-    document.getElementById("ubicacion").innerHTML = "UBICACIÓN";
+    if (document.getElementById("menuMovil").style.position === "initial") {
+        document.getElementById("inicio").style.display = "none";
+        document.getElementById("ubicacion").style.display = "none";
+        document.getElementById("about").style.display = "none";
+        document.getElementById("menuMovil").style.position = "absolute";
+    } 
 }
 
 function ubicacion() {
-    document.getElementById("inicio").innerHTML = "INICIO"
-    document.getElementById("info").innerHTML = "INFORMACIÓN";
-    document.getElementById("ubicacion").innerHTML = "···UBICACIÓN···";
+   if (document.getElementById("menuMovil").style.position === "initial") {
+        document.getElementById("inicio").style.display = "none";
+        document.getElementById("info").style.display = "none";
+        document.getElementById("about").style.display = "none";
+        document.getElementById("menuMovil").style.position = "absolute";
+    }
+}
+
+function about() {
+    if (document.getElementById("menuMovil").style.position === "initial") {
+        document.getElementById("inicio").style.display = "none";
+        document.getElementById("info").style.display = "none";
+        document.getElementById("ubicacion").style.display = "none";
+        document.getElementById("menuMovil").style.position = "absolute";
+    }
 }
